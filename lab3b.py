b639d89f565fec602b46ea63d868988be0f5cacc
MRABE05 <ma.theresedominiquerabe@gmail.com>
2024-10-03 20:37:30 -0400

    LAB3 COMPLETED

--- lab3b.py
+++ lab3b.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-'''Lab 3 Part 1 script - functions'''
+
 # Author ID: 120352224 
 
 def sum_numbers(number1, number2):
-    # Add number1 and number2 and return the value
+   
     return number1 + number2
 
 def subtract_numbers(number1, number2):
-    # Subtract number2 from number1 and return the value
+    
     return number1 - number2
 
 def multiply_numbers(number1, number2):
-    # Multiply number1 and number2 and return the value
+    
     return number1 * number2
 
 if __name__ == '__main__':
