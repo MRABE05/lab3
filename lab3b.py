76b79b39076c61a367f7cfdd1a422c94534a515a
MRABE05 <ma.theresedominiquerabe@gmail.com>
2024-10-03 20:24:50 -0400

    COMPLETED LAB3

--- lab3b.py
+++ lab3b.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Lab 3 Part 1 script - functions'''
-# Author ID: [your_seneca_id] 
+# Author ID: 120352224 
 
 def sum_numbers(number1, number2):
     # Add number1 and number2 and return the value
